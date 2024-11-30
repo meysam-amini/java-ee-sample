f14798fb192e4d815574189f30cf4d295891e3fb
meysam <meysamamini7394@gmail.com>
2024-12-01 00:00:27 +0330

    add Task entity

--- j2ee-backend-service/src/main/java/com.meysam/j2ee.backend.service/model/Task.java
+++ j2ee-backend-service/src/main/java/com.meysam/j2ee.backend.service/model/Task.java
@@ -1,4 +1,21 @@
 package com.meysam.j2ee.backend.service.model;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter@Setter@Builder
 public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+
+    private boolean completed;
 }
